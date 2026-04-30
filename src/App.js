2cc26e18a5c5dfde7b4fa32063bc1bdaaa2fcbcd
Aditya-Khoje <adityakhoje1@gmail.com>
2026-05-01 00:33:17 +0530

    1st commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <b>My First React App</b>
+      </header>
+    </div>
+  );
+}
+
+export default App;
